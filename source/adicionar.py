b2ab33906421617c3d27b02cf04c1188df2d807f
Eugenio-jkl <jklgamer120faria@gmail.com>
2026-08-02 03:59:15 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch new-teste
     Your branch is up to date with 'origin/new-teste'.
    
     Changes to be committed:
    	deleted:    data.db
    	modified:   source/adicionar.py
    	renamed:    dataBase.db -> source/dataBase.db
    	renamed:    pages/main.css -> source/static/css/main.css
    	renamed:    pages/mapa.css -> source/static/css/mapa.css
    	renamed:    pages/ofensiva.css -> source/static/css/ofensiva.css
    	renamed:    pages/perfil.css -> source/static/css/perfil.css
    	renamed:    pages/login.js -> source/static/js/login.js
    	renamed:    pages/mapa.js -> source/static/js/mapa.js
    	renamed:    pages/ofensiva.js -> source/static/js/ofensiva.js
    	renamed:    pages/perfil.js -> source/static/js/perfil.js
    	renamed:    pages/theme.js -> source/static/js/theme.js
    	renamed:    pages/Mapa.html -> source/templates/Mapa.html
    	renamed:    pages/Ofensiva.html -> source/templates/Ofensiva.html
    	renamed:    pages/Perfil.html -> source/templates/Perfil.html
    	renamed:    pages/contato.html -> source/templates/contato.html
    	renamed:    pages/dashboards/dashboard-aluno.css -> source/templates/dashboards/dashboard-aluno.css
    	renamed:    pages/dashboards/dashboard-aluno.html -> source/templates/dashboards/dashboard-aluno.html
    	renamed:    pages/dashboards/dashboard-aluno.js -> source/templates/dashboards/dashboard-aluno.js
    	renamed:    pages/dashboards/dashboard-professor.html -> source/templates/dashboards/dashboard-professor.html
    	renamed:    pages/home.html -> source/templates/home.html
    	renamed:    pages/login/login-aluno.html -> source/templates/login/login-aluno.html
    	renamed:    pages/login/login-professor.html -> source/templates/login/login-professor.html
    	renamed:    pages/register/cadastro-aluno.html -> source/templates/register/register-aluno.html
    	renamed:    pages/register/cadastro-professor.html -> source/templates/register/register-professor.html
    	renamed:    pages/teste.html -> source/templates/teste.html
    	modified:   source/teste.py

--- source/adicionar.py
+++ source/adicionar.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-con = sqlite3.connect("data.db")
+con = sqlite3.connect("dataBase.db")
 cursor = con.cursor()
 
 cursor.execute("""
@@ -28,3 +28,17 @@ con.commit()
 con.close()
 
 print("Aluno cadastrado com sucesso!")
+
+
+#isso aqui tava no teste
+#import sqlite3
+#
+#con = sqlite3.connect("data.db")
+#cursor = con.cursor()
+#
+#cursor.execute("SELECT * FROM alunos")
+#
+#for aluno in cursor.fetchall():
+#    print(aluno)
+#
+#con.close()
--- source/teste.py
+++ source/teste.py
@@ -1,11 +1,38 @@
 import sqlite3
+import flask
+#isso aqui so é util se as paginas tiverem dentro de uma pasta chamada templates(nao da pra usar caminho absolutos)
+#site = flask.Flask(__name__)
 
-con = sqlite3.connect("data.db")
-cursor = con.cursor()
+#puta merda 
+site = flask.Flask(__name__)
 
-cursor.execute("SELECT * FROM alunos")
+@site.route("/")
+def home():
+    #return flask.render_template("/workspaces/ACC-2026/pages/home.html")
+    return flask.render_template("home.html")
 
-for aluno in cursor.fetchall():
-    print(aluno)
+@site.route("/mapa")
+def mapa():
+    return flask.render_template("Mapa.html")
 
-con.close()
+@site.route("/perfil")
+def perfil():
+    return flask.render_template("Perfil.html")
+
+@site.route("/ofensiva")
+def ofensiva():
+    return flask.render_template("Ofensiva.html")
+
+@site.route("/login/aluno")
+def login_aluno():
+    return flask.render_template("login/login-aluno.html")
+#a cada segundo estou mas perto da insanidade completa
+
+@site.route("/login/professor")
+def login_professor():
+    return flask.render_template("login/login-professor.html")
+
+site.run()
+
+
+#vai tomar no cu na proxima reuniao tem que separar essas porras por pasta caralho
