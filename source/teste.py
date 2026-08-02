5e8f549e51e90e6601ef1d33475e8d3b87a6bbb2
Eugenio-jkl <jklgamer120faria@gmail.com>
2026-08-02 04:54:30 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch new-teste
     Your branch is up to date with 'origin/new-teste'.
    
     Changes to be committed:
    	modified:   source/templates/login/login-aluno.html
    	modified:   source/teste.py

--- source/teste.py
+++ source/teste.py
@@ -2,6 +2,7 @@ import sqlite3
 import flask
 #isso aqui so é util se as paginas tiverem dentro de uma pasta chamada templates(nao da pra usar caminho absolutos)
 #site = flask.Flask(__name__)
+#ja resolvi nessa branch pelo menos
 
 #puta merda 
 site = flask.Flask(__name__)
@@ -23,16 +24,32 @@ def perfil():
 def ofensiva():
     return flask.render_template("Ofensiva.html")
 
-@site.route("/login/aluno")
+@site.route("/login/aluno", methods=["GET","POST"])
 def login_aluno():
+    if flask.request.method == "POST":
+        email = flask.request.form["email"]
+        senha = flask.request.form["senha"]
+    
+        print(email)
+        print(senha)
+
     return flask.render_template("login/login-aluno.html")
+
 #a cada segundo estou mas perto da insanidade completa
 
 @site.route("/login/professor")
 def login_professor():
     return flask.render_template("login/login-professor.html")
 
+
+
+@site.route("/register/aluno")
+def register_aluno():
+    return flask.render_template("register/register-aluno.html")
+
+@site.route("/register/professor")
+def register_professor():
+    return flask.render_template("register/register-professor.html")
+
 site.run()
-
-
 #vai tomar no cu na proxima reuniao tem que separar essas porras por pasta caralho
