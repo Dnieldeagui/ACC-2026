0d1d3d77867dc2dedce34a730210afceebd887b4
Eugenio-jkl <jklgamer120faria@gmail.com>
2026-08-29 02:09:07 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch newt2
     Your branch is up to date with 'origin/newt2'.
    
     Changes to be committed:
    	modified:   dataBase.db
    	modified:   source/main.py
    	modified:   source/templates/register/register-aluno.html
    	modified:   source/templates/register/register-professor.html

--- source/main.py
+++ source/main.py
@@ -24,9 +24,12 @@ def perfil():
 def ofensiva():
     return flask.render_template("Ofensiva.html")
 
+
 @site.route("/contato")
 def contato():
     return flask.render_template("contato.html")
+
+
 @site.route("/login/aluno", methods=["GET", "POST"])
 def login_aluno():
     if flask.request.method == "POST":
@@ -89,10 +92,12 @@ def register_professor():
     
         return flask.redirect(flask.url_for("home"))
     return flask.render_template("register/register-professor.html")
+
     
 @site.route("/dashboard/professor")
 def dashboard_professor():
     return flask.render_template("dashboards/dashboard-professor.html")
+
 
 if __name__ == "__main__":
     site.run(host="0.0.0.0", port=5000, debug=True)
